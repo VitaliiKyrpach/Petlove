444362d186d48bfd020a653fc1e3b6c5cb76bb27
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-12-30 11:19:41 +0200

    add header styles

--- src/app/components/auth-nav/auth-nav.component.ts
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-auth-nav',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NgClass, RouterLink, RouterLinkActive],
   templateUrl: './auth-nav.component.html',
   styleUrl: './auth-nav.component.scss'
 })
 export class AuthNavComponent {
-
+  @Input() path:string = ''
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 import { AuthNavComponent } from '../auth-nav/auth-nav.component';
 import { UserNavComponent } from '../user-nav/user-nav.component';
+import { filter } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, IconSpriteModule, AuthNavComponent, UserNavComponent],
+  imports: [NgClass,RouterLink, RouterLinkActive, IconSpriteModule, AuthNavComponent, UserNavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  public isLoged = false
+export class HeaderComponent implements OnInit {
+  public isLoged:boolean = false
+  public path: string = ''
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if(event.url.length > 1){
+        this.path = event.url.replace('/','')
+      }else{
+        this.path = event.url
+      }
+    });
+  }
 }
