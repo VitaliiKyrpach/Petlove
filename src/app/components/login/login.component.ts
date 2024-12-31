e18f79ea4182731387f65af05651510ce488a180
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2024-12-31 11:09:43 +0200

    add login styles and logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  constructor(private router: Router) {}
+  public loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  public onSubmit() {
+    console.log(this.loginForm.value);
+  }
+  public redirect() {
+    this.router.navigate(['/registration']);
+  }
 }
