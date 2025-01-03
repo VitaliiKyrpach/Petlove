2af76aa7885fa10088b2e4e1c227820761b0efdf
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2025-01-03 11:53:16 +0200

    add validation for login and register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass, IconSpriteModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -13,13 +15,51 @@ export class LoginComponent {
   constructor(private router: Router) {}
 
   public loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)]),
+    password: new FormControl('',[Validators.required, Validators.minLength(7)]),
   });
   public onSubmit(): void {
     console.log(this.loginForm.value);
+    this.loginForm.reset()
   }
   public redirect(): void {
     this.router.navigate(['registration']);
+  }
+
+  public errors = {
+    email: '',
+    password: ''
+  }
+
+  get email(){
+    return this.loginForm.get('email') as FormControl
+  }
+  get password(){
+    return this.loginForm.get('password') as FormControl
+  }
+
+  public updErrorMsg(type: string){
+    switch(type){
+      case 'email':
+        if(this.email && this.email.hasError('required')){
+          this.errors.email = 'Email is required'
+        } else if(
+          this.email && this.email.hasError('pattern')
+        ){
+          this.errors.email = 'Email is invalid'
+        } else{
+          this.errors.email = ''
+        }
+        break;
+      case 'password':
+        if(this.password && this.password.hasError('required')){
+          this.errors.password = 'Password is required'
+        } else if(this.password && this.password.hasError('minlength')){
+          this.errors.password = 'Password must contain minimum 7 symbols'
+        } else{
+          this.errors.password = ''
+        }
+        break;
+    }
   }
 }
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,12 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule, IconSpriteModule],
+  imports: [ReactiveFormsModule, IconSpriteModule, NgClass],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
@@ -17,14 +18,15 @@ export class RegistrationComponent {
     confirm: false,
   };
   public regForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirm: new FormControl(''),
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(7)]),
+    confirm: new FormControl('', [Validators.required]),
   });
 
   public onSubmit(): void {
     console.log(this.regForm.value);
+    this.regForm.reset()
   }
 
   public redirect(): void {
@@ -32,5 +34,66 @@ export class RegistrationComponent {
   }
   public handleShown(type: 'pass' | 'confirm') {
     this.isShown[type] = !this.isShown[type];
+  }
+
+  public errors = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: ''
+  }
+
+  get name(){
+    return this.regForm.get('name') as FormControl
+  }
+  get email(){
+    return this.regForm.get('email') as FormControl
+  }
+  get password(){
+    return this.regForm.get('password') as FormControl
+  }
+  get confirm(){
+    return this.regForm.get('confirm') as FormControl
+  }
+
+  public confirmPass(){
+    console.log(this.password.value)
+  }
+  public updErrorMsg(type: string){
+    switch (type){
+      case 'name':
+        if(this.name && this.name.hasError('required')){
+          this.errors.name = 'Name is required'
+        }else{
+          this.errors.name = ''
+        }
+        break;
+      case 'email':
+        if(this.email && this.email.hasError('required')){
+          this.errors.email = 'Email is required'
+        } else if(this.email && this.email.hasError('pattern')){
+          this.errors.email = 'Email is invalid'
+        }else{
+          this.errors.email = ''
+        }
+        break;
+      case 'password':
+        if(this.password && this.password.hasError('required')){
+          this.errors.password = 'Password is required'
+        } else if( this.password && this.password.hasError('minlength')){
+          this.errors.password = 'Password must contain minimum 7 symbols'
+        } else {
+          this.errors.password = ''
+        }
+        break;
+      case 'confirm':
+        if(this.confirm && this.confirm.hasError('required')){
+          this.errors.confirm = 'Confirmation password is required'
+        }else if(this.confirm.value !== this.password.value){
+          this.errors.confirm = 'Confirmation password does not match'
+        } else{
+          this.errors.confirm = ''
+        }
+    }
   }
 }
