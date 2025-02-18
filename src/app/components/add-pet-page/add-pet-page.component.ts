6fa87d1b63a0545eb7d33bca7cd80cca88a77f73
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2025-02-18 11:34:03 +0200

    add some styles for addPet Page

--- src/app/components/add-pet-page/add-pet-page.component.ts
+++ src/app/components/add-pet-page/add-pet-page.component.ts
@@ -3,31 +3,44 @@ import { ServiceService } from '../../services/service.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
-import { NgLabelTemplateDirective, NgOptionTemplateDirective, NgSelectComponent, NgSelectModule } from '@ng-select/ng-select';
+import {
+  NgLabelTemplateDirective,
+  NgOptionTemplateDirective,
+  NgSelectComponent,
+  NgSelectModule,
+} from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pet-page',
   standalone: true,
-  imports: [ AsyncPipe, IconSpriteModule, NgLabelTemplateDirective,
-      NgOptionTemplateDirective,
-      NgSelectComponent,
-      NgSelectModule,
-    CommonModule, FormsModule],
+  imports: [
+    AsyncPipe,
+    IconSpriteModule,
+    NgLabelTemplateDirective,
+    NgOptionTemplateDirective,
+    NgSelectComponent,
+    NgSelectModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './add-pet-page.component.html',
-  styleUrl: './add-pet-page.component.scss'
+  styleUrl: './add-pet-page.component.scss',
 })
 export class AddPetPageComponent implements OnInit {
-  public species$!: Observable<string[]>
-  public sex!: string; 
+  public species$!: Observable<string[]>;
+  public sex!: string;
   public chevron: boolean = false;
-  constructor (private service: ServiceService){}
+  constructor(private service: ServiceService) {}
   ngOnInit(): void {
-    this.species$ = this.service.getSpecies()
+    this.species$ = this.service.getSpecies();
   }
-  public handleChevron(){
-    this.chevron = !this.chevron
+  public handleChevron() {
+    this.chevron = !this.chevron;
   }
-  public handlepick(){}
-  public onFileChange(file: any){}
+  public handlepick() {}
+  public onFileChange(file: any) {}
+  public showSex() {
+    console.log(this.sex);
+  }
 }
